cb89b5c675cb7c0bcec998ebb249f656011f4ae7
lizhihong <joker_lizhih@163.com>
2020-12-02 18:19:35 +0800

    add some comments

--- src/store/models/Product.ts
+++ src/store/models/Product.ts
@@ -7,7 +7,10 @@ const state: Products = {
 }
 
 const getters = {
-  products: (state: any) => {
+  // 作为Property-Style Access的getters会作为Vue's reactivity system的一部分进行缓存
+  // 如果是通过Method-Style Access的getters，只有当调用时才会执行，且结果不会进行缓存
+  // e.g. products: (state: any) => { return state.all }
+  products (state: any) {
     return state.all
   }
 }
